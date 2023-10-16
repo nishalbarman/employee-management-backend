e0403284013dbd0d6e78f8ab6cf5b97229ae6a68
Nishal Barman <helptechnoapps@gmail.com>
2023-10-16 23:58:35 +0530

    Backend server

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const port = process.env.PORT || 8000;
 const app = express();
+const cors = require("cors");
 require("./mongo/connect");
 
-app.use(require("cors")());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use("/auth", require("./routes/auth.routes"));
 app.use("/employees", require("./routes/employee.routes"));
